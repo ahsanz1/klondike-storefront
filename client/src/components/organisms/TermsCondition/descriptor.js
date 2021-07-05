281a0036033c6ad930115c1dfc20adf37e413f86
bilal531 <bilal.ilyas@shopdev.co>
2021-07-05 18:01:58 +0500

    change descriptor

--- client/src/components/organisms/TermsCondition/descriptor.js
+++ client/src/components/organisms/TermsCondition/descriptor.js
@@ -2,6 +2,7 @@ const { Types } = require('@teamfabric/xpm')
 exports.default = Types.Component({
   id: 'TermsCondition',
   label: 'Legal',
+  isGlobal: false,
   description: `Legal component where each child has 4 text options and boolean attributes for 
   displaying text as paragraph or list`,
   attributes: {
