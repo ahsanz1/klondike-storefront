36eedec0cc0d973b6e2c9f4783f234899e25efa2
Tauqeer <tauqeer.taj@gmail.com>
2021-08-30 15:21:19 +0500

    sidebar fix

--- client/src/components/organisms/plp/descriptor.js
+++ client/src/components/organisms/plp/descriptor.js
@@ -1,7 +1,7 @@
 const { Types } = require('@teamfabric/xpm')
 
 exports.default = Types.Component({
-  id: 'PLP-Page',
+  id: 'PLP',
   label: 'PLP-Page',
   isGlobal: false,
   description: 'Product Listing Page',
