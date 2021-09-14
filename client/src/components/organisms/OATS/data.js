fecdfc8e37b986b2ea30da38ec194560eda81e4c
zahid khubaib <88072627+zahidkhubaib123@users.noreply.github.com>
2021-09-14 17:49:42 +0500

    call api data

--- client/src/components/organisms/OATS/data.js
+++ client/src/components/organisms/OATS/data.js
@@ -14,34 +14,4 @@ export const tableOatsData = {
       value: 'truck',
     },
   ],
-  // tableData: [
-  //   {
-  //     category: 'Proprietary transmissions',
-  //     manufacturer: 'Deutz',
-  //     modal: 'bf4m10113fc<dsadas<fdafasvehicle Engine (average speed 60km)',
-  //     year: '2015',
-  //     fuel: 'DT',
-  //   },
-  //   {
-  //     category: 'Proprietary transmissions',
-  //     manufacturer: 'Deutz',
-  //     modal: 'bf4m10113fc<dsadas<fdafasvehicle Engine (average speed 60km)',
-  //     year: '2019',
-  //     fuel: 'DT',
-  //   },
-  //   {
-  //     category: 'Proprietary transmissions',
-  //     manufacturer: 'Deutz',
-  //     modal: 'bf4m10113fc<dsadas<fdafasvehicle Engine (average speed 60km)',
-  //     year: '2014',
-  //     fuel: 'DT',
-  //   },
-  //   {
-  //     category: 'Proprietary transmissions',
-  //     manufacturer: 'Deutz',
-  //     modal: 'bf4m10113fc<dsadas<fdafasvehicle Engine (average speed 60km)',
-  //     year: '2025',
-  //     fuel: 'DT',
-  //   },
-  // ],
 }
--- client/src/components/organisms/OATS/index.js
+++ client/src/components/organisms/OATS/index.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 import './style.scss'
-// import axios from 'axios'
+import axios from 'axios'
 
-import InputTextField from 'components/atoms/input'
 import Dropdown from 'components/atoms/dropdown'
 import { tableOatsData } from './data'
 import Button from 'components/atoms/button'
@@ -10,24 +9,26 @@ import Button from 'components/atoms/button'
 const Oats = () => {
   const { mainHeading, listItem } = tableOatsData
 
-  // const [otsdata, setOtsdata] = useState()
-  const [query, setQuery] = useState()
-  // const getproducts = () => {
-  //   const url =
-  //     'https://klondike-ws-canada.phoenix.earlweb.net/search?q=audi&token=LiEoiv0tqygb'
+  const [otsdata, setOtsdata] = useState([])
+  const [query, setQuery] = useState('')
 
-  //   axios
-  //     .get(url)
-  //     .then(response => {
-  //       let results = response.data.equipment_list
-  //       setOtsdata(results)
-  //     })
-  //     .catch(err => console.log('error '))
-  // }
-
-  // useEffect(() => {
-  //   getproducts()
-  // }, [])
+  const getproducts = () => {
+    const url = [
+      `https://klondike-ws-canada.phoenix.earlweb.net/search?q=${query}&token=LiEoiv0tqygb`,
+    ]
+    axios.get(url).then(response => {
+      let results = response.data.equipment_list
+      setOtsdata(results)
+    })
+  }
+  const filterData = e => {
+    setQuery(e.target.value)
+  }
+  const searchQuery = () => {
+    if (query) {
+      getproducts()
+    }
+  }
 
   return (
     <>
@@ -35,19 +36,17 @@ const Oats = () => {
         <div className="oats">
           <h1 className="heading">{mainHeading}</h1>
           <div className="wrapper">
-            <InputTextField
-              value={query}
-              onChange={({ value }) => {
-                setQuery(value)
-              }}
+            <input
+              onChange={filterData}
               className="input_model"
               placeholder="Enter code  model"
+              value={query}
             />
             <div className="wrapper-two">
               <Dropdown className="cars" items={listItem} />
 
               <div className="search_bar">
-                <Button className="btn-search">
+                <Button className="btn-search" onClick={searchQuery}>
                   <img
                     src="/static/images/Frame.png"
                     alt="icon"
@@ -58,46 +57,44 @@ const Oats = () => {
             </div>
           </div>
 
-          {/* <div className="overflow">
-
-        <div className="dropdown-wrapper">
-          <Dropdown className="year_range" />
-          <Dropdown className="series year_range" />
-          <Dropdown className="family year_range" />
-          <Dropdown className="manufecturer year_range" />
-        </div>
-
-        <div className="table-wrapper">
-          <div className="title flex">
-            <h3 className="custom-grid">Category</h3>
-            <h3 className="custom-grid">Manufacturer</h3>
-            <h3 className="custom-grid">Model</h3>
-            <h3 className="custom-grid">Year</h3>
-            <h3 className="custom-grid">Fuel</h3>
+          <div className="dropdown-wrapper">
+            <Dropdown className="year_range" />
+            <Dropdown className="series " />
+            <Dropdown className="family " />
+            <Dropdown className="manufecturer " />
           </div>
-            {otsdata &&
-            otsdata.equipment &&
-            otsdata.equipment.map((data, i) => {
-              const {
-                productgroup,
-                manufacturer,
-                model,
-                yearto,
-                alt_fueltype,
-              } = data
-              return (
-                <div className="table-content flex" key={i}>
+          <div className="overflow">
+            <div className="table-wrapper">
+              <div className="title flex">
+                <h3 className="custom-grid">Category</h3>
+                <h3 className="custom-grid">Manufacturer</h3>
+                <h3 className="custom-grid">Model</h3>
+                <h3 className="custom-grid">Year</h3>
+                <h3 className="custom-grid">Fuel</h3>
+              </div>
+              {otsdata &&
+                otsdata.equipment &&
+                otsdata.equipment.map((data, i) => {
+                  const {
+                    productgroup,
+                    manufacturer,
+                    model,
+                    year,
+                    fueltype,
+                  } = data
 
-                    <p className="custom-grid">{productgroup}</p>
-                  <p className="custom-grid">{manufacturer}</p>
-                  <p className="custom-grid">{model}</p>
-                  <p className="custom-grid">{yearto}</p>
-                  <p className="custom-grid">{alt_fueltype}</p>
-                </div>
-              )
-            })}
-        </div>
-        </div> */}
+                  return (
+                    <div className="table-content flex" key={i}>
+                      <p className="custom-grid">{productgroup}</p>
+                      <p className="custom-grid">{manufacturer}</p>
+                      <p className="custom-grid">{model}</p>
+                      <p className="custom-grid">{year}</p>
+                      <p className="custom-grid">{fueltype}</p>
+                    </div>
+                  )
+                })}
+            </div>
+          </div>
         </div>
       </div>
     </>
