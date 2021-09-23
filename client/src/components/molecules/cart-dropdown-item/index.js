f56e324a214d43cb181d6776f6895e7018a211f7
Muhammad Fahad <mfba@Fahads-Brain-companion.local>
2021-09-23 16:54:41 +0500

    created cart without mobile responsive

--- client/src/components/molecules/cart-dropdown-item/index.js
+++ client/src/components/molecules/cart-dropdown-item/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import './styles.scss'
@@ -6,7 +6,8 @@ import Label from 'components/atoms/label'
 import Image from 'components/atoms/image'
 import Button from 'components/atoms/button'
 
-import useRemoveFromCart from 'libs/api-hooks/useRemoveFromCart'
+import { InputNumber } from 'antd'
+// import useRemoveFromCart from 'libs/api-hooks/useRemoveFromCart'
 
 const CartDropdownItem = ({
   mainImage,
@@ -17,49 +18,94 @@ const CartDropdownItem = ({
   lineItemId,
   totalBars,
 }) => {
-  const [removing, setRemoving] = useState(false)
-  const { removeFromCart, error } = useRemoveFromCart()
+  // const [removing, setRemoving] = useState(false)
+  // const { removeFromCart, error } = useRemoveFromCart()
 
-  const removeItem = async () => {
-    setRemoving(true)
-    await removeFromCart(lineItemId)
-    setRemoving(false)
-    if (!error || error) {
-      setRemoving(false)
-    }
-  }
+  // const removeItem = async () => {
+  //   setRemoving(true)
+  //   await removeFromCart(lineItemId)
+  //   setRemoving(false)
+  //   if (!error || error) {
+  //     setRemoving(false)
+  //   }
+  // }
 
   return (
-    <div className="cart-item">
-      <div className="cart-item-image">
-        <Image src={mainImage} />
-      </div>
-      <div className="cart-item-details">
-        <Label className="cart-item-title">{title}</Label>
-        <div className="cart-item-subdetails">
-          {size && (
-            <Label className="cart-item-subdetails-label"> size: {size}</Label>
-          )}
-          {totalBars && (
-            <Label className="cart-item-subdetails-label">{`${totalBars} bars`}</Label>
-          )}
-          <Label className="cart-item-subdetails-label">
-            {' '}
-            Quantity: {quantity}
-          </Label>
+    <div className="mini-cart-item">
+      <div className="cart-item">
+        <div>
+          <Image src={mainImage} className="cart-item-image" />
         </div>
-      </div>
-      <div className="cart-item-pricing">
-        <Label className="cart-item-pricing-label">
-          ${(quantity * price).toFixed(2)}
-        </Label>
-        <Button
-          onClick={removeItem}
-          disabled={removing && true}
-          className="cart-item-pricing-label"
-        >
-          {removing ? 'Removing' : 'Remove'}
-        </Button>
+        <div>
+          <div className="item-desc-and-price">
+            <div className="item-desc">
+              <Label className="item-title">{title}</Label>
+              <div className="product-detail-info-cart">
+                <Label className="item-info">
+                  SIZE: <p className="item-subInfo">{size}</p>
+                </Label>
+
+                <Label className="item-info">
+                  PER CASE: <p className="item-subInfo">{price}</p>
+                </Label>
+                <Label className="item-info">
+                  PART NUM:
+                  {/* <p className="item-subInfo">{props['Part Number']}</p> */}
+                </Label>
+              </div>
+            </div>
+
+            <div className="product-price-info">
+              <Label className="product-price">
+                <p className="product-price-mobile">PRICE</p>${price}
+              </Label>
+            </div>
+          </div>
+          <div className="total-and-quantity-cart">
+            <div className="removebtn-div">
+              <Button
+                className="remove-button"
+                //   onClick={removeItem}
+                //   disabled={removing && true}
+              >
+                {/* {removing ? 'Removing' : 'Remove'} */}
+                Remove
+              </Button>
+            </div>
+            <div className="quantity-box">
+              {/* <Label className="items-quanity">{quantity}</Label>
+            <div className="quantity-buttons">
+              <ImageOnlyButton
+                onClick={() => updateCartApiCall(itemId, 1)}
+                id={itemId}
+                imgSrc="/static/icons/arrow-up.png"
+                // imgAlt={imageAlt}
+                className="quantity-increase-btn"
+              />
+              <ImageOnlyButton
+                onClick={() => updateCartApiCall(itemId, -1)}
+                id={itemId}
+                imgSrc="/static/icons/arrow-down.png"
+                disabled={quantity === 1}
+                className="quantity-increase-btn"
+              />
+            </div> */}
+
+              <p className="product-quantity-mobile">QTY:</p>
+              <InputNumber
+                className="product-quantity-spinner"
+                min={1}
+                max={1000}
+                defaultValue={quantity}
+                // onChange={onChange}
+              />
+            </div>
+            <Label className="total-price">
+              <p className="product-total-mobile">TOTAL PRICE</p>$
+              {(quantity * price).toFixed(2)}
+            </Label>
+          </div>
+        </div>
       </div>
     </div>
   )
