631672b40defc39cd80f7c35cfadebc032ca2daf
Tauqeer <tauqeer.taj@gmail.com>
2021-10-05 11:21:57 +0500

    fix: updates search filter

--- client/src/components/organisms/searchFilter/index.js
+++ client/src/components/organisms/searchFilter/index.js
@@ -1,13 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from 'libs/context'
 import { fetchItems } from 'libs/services/algolia'
-const SearchFilter = () => {
+const SearchFilter = props => {
   const [filter, setFilter] = useState([])
   const { searchFilter } = useContext(AppContext)
   console.log('check filter array:', searchFilter)
   useEffect(() => {
     const list = fetchItems(searchFilter)
-    console.log('array check:', list)
+    console.log('array check:', props)
     setFilter(list.hits)
   }, [])
   console.log('filter array:', filter)
