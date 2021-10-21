a59fe655539b6847462581d7033b9e15142e0944
Tauqeer <tauqeer.taj@gmail.com>
2021-10-21 17:29:08 +0500

    fix: bugs 10/21/2021

--- client/src/components/molecules/selectedPlpCategory/index.js
+++ client/src/components/molecules/selectedPlpCategory/index.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from 'components/atoms/button'
+import PropTypes from 'prop-types'
 import './style.scss'
 
-const selectedPlpCategory = ({ name, desc }) => {
+const SelectedPlpCategory = ({ name, desc }) => {
+  const [readMore, setReadMore] = useState(200)
+  const truncate = (str, maxlength) => {
+    return str.length > maxlength ? str.slice(0, maxlength - 1) + '…' : str
+  }
+  const more = () => {
+    setReadMore(desc.length)
+  }
   return (
     <div className="selected-category">
       <h2>{name}</h2>
-      <p>{desc}</p>
+      <p>{truncate(desc, readMore)}</p>
+      {desc.length > 0 && readMore !== desc.length && (
+        <Button onClick={more}>Read more</Button>
+      )}
     </div>
   )
 }
-export default selectedPlpCategory
+SelectedPlpCategory.propTypes = {
+  name: PropTypes.string,
+  desc: PropTypes.array,
+}
+export default SelectedPlpCategory
