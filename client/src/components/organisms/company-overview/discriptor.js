7e9c12dfe014dfc4b7e8105ba09b991b8cb834ce
Tauqeer <tauqeer.taj@gmail.com>
2021-10-26 16:31:36 +0500

    fix: descriptor-over-view

--- client/src/components/organisms/company-overview/discriptor.js
+++ client/src/components/organisms/company-overview/discriptor.js
@@ -1,52 +1,28 @@
-// const { Types } = require('@teamfabric/xpm')
-
-// exports.default = Types.Component({
-//   id: 'OverView',
-//   label: 'Company Over View Component',
-//   isGlobal: false,
-//   description: `Company Over-View component contains the description about each heading and the bottom section contains button that will redirect you to specific page for futher details`,
-
-//   attributes: {
-//     mainHeading: Types.String({ label: 'Main Heading' }),
-//     commited: Types.Array({
-//       label: 'Company Over-View Section Content',
-//       children: Types.Shape({
-//         children: {
-//           subHeading: Types.String({ label: 'Sub Heading' }),
-//           subSection: Types.Array({
-//             label: 'Description-Section',
-//             Children: Types.Shape({
-//               children: {
-//                 paragraph: Types.String({ label: 'Description' }),
-//               },
-//             }),
-//           }),
-//           // butontext: Types.String({ label: 'Button Text' }),
-//           // redirectUrl: Types.String({ label: 'Redirect URL' }),
-//         },
-//       }),
-//     }),
-//   },
-// })
 const { Types } = require('@teamfabric/xpm')
 
 exports.default = Types.Component({
-  id: 'CommitedSuccess',
-  label: 'CommitedSuccess Component222',
+  id: 'OverView',
+  label: 'Company Over View Component',
   isGlobal: false,
-  description: `Commited component where each child has 4 text options and boolean attributes for 
-  displaying text as paragraph or dialogue`,
+  description: `Company Over-View component contains the description about each heading and the bottom section contains button that will redirect you to specific page for futher details`,
 
   attributes: {
+    mainHeading: Types.String({ label: 'Main Heading' }),
     commited: Types.Array({
-      label: 'Commited Section Content',
+      label: 'Company Over-View Section Content',
       children: Types.Shape({
         children: {
-          heading: Types.String({ label: 'Heading' }),
-          subheading: Types.String({ label: 'Sub Heading' }),
-          paragraph: Types.String({ label: 'Text' }),
-          butontext: Types.String({ label: 'Button Text' }),
-          redirectUrl: Types.String({ label: 'Redirect URL' }),
+          subHeading: Types.String({ label: 'Sub Heading' }),
+          subSection: Types.Array({
+            label: 'Description-Section',
+            Children: Types.Shape({
+              children: {
+                paragraph: Types.String({ label: 'Description' }),
+              },
+            }),
+          }),
+          // butontext: Types.String({ label: 'Button Text' }),
+          // redirectUrl: Types.String({ label: 'Redirect URL' }),
         },
       }),
     }),
