76401d21523f90dd38b2da079404ff93194ed078
bilal531 <bilal.ilyas@shopdev.co>
2021-10-27 21:22:49 +0500

    PCP Bottom Descriptor

--- client/src/components/organisms/pcpBottom/descriptor.js
+++ client/src/components/organisms/pcpBottom/descriptor.js
@@ -6,20 +6,13 @@ exports.default = Types.Component({
   isGlobal: false,
   description: 'pcp bottom section',
   attributes: {
-    plpBottom: Types.Array({
-      label: 'PCP-Bottom',
-      children: Types.Shape({
-        children: {
-          plpBottomImage: Types.Image({
-            label: 'Image',
-            url: Types.String({ label: 'URL' }),
-            altText: Types.String({ label: 'Alt text' }),
-          }),
-          button: Types.String({ label: 'Button Text' }),
-          url: Types.String({ label: 'URL' }),
-          mobileButton: Types.String({ label: 'Mobile Button Text' }),
-        },
-      }),
+    image: Types.Image({
+      label: 'Image',
+      url: Types.String({ label: 'URL' }),
+      altText: Types.String({ label: 'Alt text' }),
     }),
+    button: Types.String({ label: 'Button Text' }),
+    url: Types.String({ label: 'Button URL' }),
+    mobileButton: Types.String({ label: 'Mobile Button Text' }),
   },
 })
