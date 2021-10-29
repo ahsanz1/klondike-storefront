3cc94c75ac323dc3fc571a21494d930019250c5a
Bilal Qamar <bilal.qamar@shopdev.co>
2021-10-29 11:15:13 +0500

    KLON-459 bugs resolved

--- client/src/components/organisms/catlog-page/index.js
+++ client/src/components/organisms/catlog-page/index.js
@@ -94,7 +94,6 @@ const Catlog = ({ heading, catData, image }) => {
                     )
                   })}
                 <br />
-                <br />
                 Download an electronic copy of the
                 <Link
                   className="english-link"
