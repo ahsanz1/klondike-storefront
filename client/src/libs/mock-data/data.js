23a98e674aacfdd67901e3367156fff2b4d7af9f
Muhammad Fahad <mfba@192.168.10.9>
2021-10-30 19:06:32 +0500

    video gallery and our-story page descriptors

--- client/src/libs/mock-data/data.js
+++ client/src/libs/mock-data/data.js
@@ -82,6 +82,9 @@ import {
   warrantyData,
   overviewData,
   oamdata,
+  technicalBanner,
+  bigVideodata,
+  smallVideodata,
 } from 'libs/data/data'
 const mockData = {
   // '/': [
@@ -570,10 +573,19 @@ const mockData = {
   ],
   '/tech-resources/video-gallery': [
     {
-      id: 'VideoGallery',
-      params: {},
+      id: 'WebpageGlobalHeroBanner',
+      params: { ...technicalBanner, key: '112233446677' },
     },
+    {
+      id: 'VideoGallery',
+      params: { bigVideodata, smallVideodata },
+    },
+    // {
+    //   id: 'PCPBottom',
+    //   params: { ...PcpBottom },
+    // },
   ],
+
   '/Techtabllist': [
     {
       id: 'Techtabllist',
