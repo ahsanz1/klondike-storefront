cd38a3a702970b939f9ec7803cf76827b2363f5b
Tauqeer <tauqeer.taj@gmail.com>
2021-11-04 12:16:17 +0500

    fix: style file error in industry

--- client/src/components/molecules/industry-applications-mobile-tablist/index.js
+++ client/src/components/molecules/industry-applications-mobile-tablist/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Label from 'components/atoms/label'
-import './style.scss'
 import { IndustryAppXPM } from 'components/organisms/Technical-tablist/data'
 import Link from 'components/atoms/link'
 
