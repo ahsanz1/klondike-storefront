bfe8dfccad26d67ec849121be855012a323f3acb
UmairKhan <umair.baloch@shopdev.co>
2021-11-19 15:28:29 +0500

    price list button disable issue fixesd

--- client/src/components/molecules/productaccordian/index.js
+++ client/src/components/molecules/productaccordian/index.js
@@ -15,9 +15,12 @@ import { AppContext } from 'libs/context'
 
 const ProductAccordion = ({ question }) => {
   // const { tableData } = tableProAccoData
-  const { user, showcartPOPModal, setGetCartItemsState } = useContext(
-    AppContext,
-  )
+  const {
+    user,
+    showcartPOPModal,
+    setGetCartItemsState,
+    getCartItems,
+  } = useContext(AppContext)
   let [qty, setQty] = useState(1)
   let [totalPrice, setTotalPrice] = useState('')
   let [modalData, setModalData] = useState('')
@@ -25,20 +28,36 @@ const ProductAccordion = ({ question }) => {
   let [showAddToCart, setShowAddToCart] = useState(false)
   let [itemId, setItemId] = useState(0)
   const [size] = useWindowSize()
-
   const [itemdata, setItemData] = useState([])
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [isPackage, setIsPackage] = useState(false)
+  const [hasCartData, setHasCartData] = useState(false)
 
   useEffect(() => {
     const data = async () => {
       const items = await fetchCategory(question)
       setItemData(items.hits)
-      // console.log('item', items)
     }
-
     data()
   }, [])
 
-  const [isModalVisible, setIsModalVisible] = useState(false)
+  useEffect(() => {
+    if (getCartItems && getCartItems.items.length > 0) {
+      let res = false
+      res = getCartItems.items[0].attributes.find(
+        arr => arr.name === 'Packaged Order',
+      )
+
+      if (res && res.value) {
+        setIsPackage(true)
+      } else {
+        setIsPackage(false)
+      }
+      setHasCartData(true)
+    } else {
+      setHasCartData(false)
+    }
+  }, [getCartItems])
 
   const showModal = data => {
     let payload = {}
@@ -57,10 +76,9 @@ const ProductAccordion = ({ question }) => {
       }`,
       sku: data['SKU'],
     }
-    console.log('payload:', payload)
+
     setModalData(payload)
     setTotalPrice(payload.totalPrice)
-
     setIsModalVisible(true)
   }
 
@@ -126,7 +144,7 @@ const ProductAccordion = ({ question }) => {
   // const onOk = () => {
   //   setIsModalVisible(false)
   // }
-  console.log('modal data:', modalData)
+
   return (
     <>
       {/* <Panel header="This is panel header 2" key="2"> */}
@@ -199,6 +217,11 @@ const ProductAccordion = ({ question }) => {
                           <Button
                             onClick={e => showModal(JSON.stringify(data))}
                             className="hover-button"
+                            disabled={
+                              hasCartData
+                                ? !(isPackage === data['Packaged Order'])
+                                : false
+                            }
                           >
                             ADD TO CART
                           </Button>
